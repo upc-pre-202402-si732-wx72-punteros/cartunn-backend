fd7110d66d5e20b4acc1166f659f2fe8fb7b0c9c
abrancitouu <abrahamayquipa2000@gmail.com>
2024-09-22 23:36:14 -0500

    chore: update the cors origin

--- src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
+++ src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
@@ -2,12 +2,30 @@ package com.thecoders.cartunnbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class CartunnBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartunnBackendApplication.class, args);
+    }
+
+    @Configuration
+    public static class Myconfiguration{
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedOrigins("https://cartunn-backend-production.up.railway.app")
+                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                            .allowedHeaders("*");
+                }
+            };
+        }
     }
 }
