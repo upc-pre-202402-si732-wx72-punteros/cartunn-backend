8223cc19ed8310f3e335c2a32dd5847dddb2c88b
abrancitouu <abrahamayquipa2000@gmail.com>
2024-09-22 22:27:23 -0500

    chore: delete the cors configurations

--- src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
+++ src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
@@ -2,30 +2,12 @@ package com.thecoders.cartunnbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CartunnBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartunnBackendApplication.class, args);
-    }
-
-    @Configuration
-    public static class Myconfiguration{
-        @Bean
-        public WebMvcConfigurer corsConfigurer() {
-            return new WebMvcConfigurer() {
-                @Override
-                public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/**")
-                            .allowedOrigins("http://localhost:3306", "http://localhost:3306/","http://localhost:4200", "http://localhost:4200/", "https://cartunn-backend-production.up.railway.app", "https://cartunn-backend-production.up.railway.app/", "https://cartunn-backend-production.up.railway.app/swagger-ui/index.html#", "https://cartunn-backend-production.up.railway.app/swagger-ui/index.html#/")
-                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
-                            .allowedHeaders("*");
-                }
-            };
-        }
     }
 }
