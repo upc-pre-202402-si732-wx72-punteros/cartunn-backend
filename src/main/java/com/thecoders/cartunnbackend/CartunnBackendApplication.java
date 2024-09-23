cb1c6413769fd6e64b24a31f408694a9c1b4c571
abrancitouu <abrahamayquipa2000@gmail.com>
2024-09-22 20:08:12 -0500

    chore: disable the cors configuration

--- src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
+++ src/main/java/com/thecoders/cartunnbackend/CartunnBackendApplication.java
@@ -2,10 +2,30 @@ package com.thecoders.cartunnbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CartunnBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartunnBackendApplication.class, args);
+    }
+
+    @Configuration
+    public static class Myconfiguration{
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedOrigins("*")
+                            .allowedMethods("*")
+                            .allowedHeaders("*");
+                }
+            };
+        }
     }
 }
